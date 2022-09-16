b3e3e584f9200a4cc0d6b70f5a8d953fea28a075
ari7946 <mercado7946@gmail.com>
2022-09-15 22:04:44 -0700

    Update loading spinner width and height, add top margin

--- features/ui/spinner/spinner.tsx
+++ features/ui/spinner/spinner.tsx
@@ -3,28 +3,28 @@ import { color, space } from "@styles/theme";
 
 const Container = styled.div`
   width: 100%;
-  height: 27vh;
+  margin-top: calc(${space(20)} + ${space(10)});
   display: flex;
   justify-content: center;
   align-items: center;
 `;
 
 const spin = keyframes`
+  from {
+    transform: rotate(0);
+  }
   to {
     transform: rotate(360deg);
-    -webkit-transform: rotate(360deg);
   }
 `;
 
 const SpinnerOutline = styled.div`
-  width: ${space(12)};
-  height: ${space(12)};
+  width: ${space(16)};
+  height: ${space(16)};
   border: ${space(2)} solid ${color("primary", 50)};
   border-radius: 50%;
   border-right-color: ${color("primary", 600)};
-  border-right-radius: 20px;
   animation: ${spin} 0.2s ease-in-out infinite;
-  -webkit-animation: ${spin} 0.4s ease-in-out infinite;
   margin: 0 auto;
 `;
 
