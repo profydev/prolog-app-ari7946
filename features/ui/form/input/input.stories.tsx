6894eac8f69485558d8a8a037e9711f6e1549b02
ari7946 <mercado7946@gmail.com>
2022-09-20 23:26:25 -0700

    Add error states, error icon to Input component

---
+++ features/ui/form/input/input.stories.tsx
@@ -0,0 +1,38 @@
+import React, { useState } from "react";
+import { ComponentStory, ComponentMeta } from "@storybook/react";
+import { Input } from "./";
+
+export default {
+  title: "UI/Input",
+  component: Input,
+  parameters: {
+    layout: "fullscreen",
+  },
+} as ComponentMeta<typeof Input>;
+
+const Template: ComponentStory<typeof Input> = (props) => {
+  const [value, setValue] = useState<string>("");
+  const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setValue(e.target.value);
+  };
+
+  return (
+    <div style={{ padding: 50 }}>
+      <Input {...props} handleChange={handleChange} value={value} />
+    </div>
+  );
+};
+export const Default = Template.bind({});
+Default.args = {
+  disabled: false,
+  iconSrc: "./icons/email-icon.svg",
+  placeholder: "olivia@untitledui.com",
+  label: "Email",
+  displayLabel: false,
+  hint: "This is a hint text to help user.",
+  error: false,
+  errorMessage: "This is an error message.",
+};
+Default.parameters = {
+  viewMode: "docs",
+};
