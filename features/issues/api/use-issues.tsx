50b3e35d3ef95ef536d4c1415260c48d69fe6e6e
ari7946 <mercado7946@gmail.com>
2022-09-23 15:29:50 -0700

    Set hardcoded filters, testing out API

--- features/issues/api/use-issues.tsx
+++ features/issues/api/use-issues.tsx
@@ -1,20 +1,32 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 import type { Page } from "@typings/page.types";
 import type { Issue } from "../types/issue.types";
 
-async function getIssues(page: number) {
+async function getIssues(page: number, filters: Filters) {
   const { data } = await axios.get(
-    `https://prolog-api.profy.dev/issue?page=${page}`
+    `https://prolog-api.profy.dev/issue?page=${page}`,
+    { params: filters }
   );
+  console.log("data", data);
   return data;
 }
 
+type Filters = {
+  status: null | "open" | "resolved";
+  level: null | "error" | "warning" | "info";
+};
+
 export function useIssues(page: number) {
+  const [filters, setFilters] = useState<Filters>({
+    status: "open",
+    level: null,
+  });
+
   const query = useQuery<Page<Issue>, Error>(
-    ["issues", page],
-    () => getIssues(page),
+    ["issues", page, filters],
+    () => getIssues(page, filters),
     {
       keepPreviousData: true,
       staleTime: 60000,
@@ -25,10 +37,11 @@ export function useIssues(page: number) {
   const queryClient = useQueryClient();
   useEffect(() => {
     if (query.data?.meta.hasNextPage) {
-      queryClient.prefetchQuery(["projects", page + 1], () =>
-        getIssues(page + 1)
+      queryClient.prefetchQuery(["projects", page + 1, filters], () =>
+        getIssues(page + 1, filters)
       );
     }
-  }, [query.data, page, queryClient]);
+  }, [query.data, page, queryClient, filters]);
+
   return query;
 }
