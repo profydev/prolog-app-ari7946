672cff26c4d5b1502d89eb45c7e77ab924b0fda5
ari7946 <mercado7946@gmail.com>
2022-09-27 17:45:33 -0700

    Get project names dynamically instead of hardcoded

--- features/issues/components/filters/filters.tsx
+++ features/issues/components/filters/filters.tsx
@@ -2,6 +2,7 @@ import React, { useState, useEffect, useCallback } from "react";
 import styled from "styled-components";
 import { Select, Option, Input } from "@features/ui";
 import { useFilters, IssueLevel, IssueStatus } from "@features/issues";
+import { useProjects } from "@features/projects";
 
 const Container = styled.div`
   padding-bottom: 2rem;
@@ -12,9 +13,10 @@ const Container = styled.div`
 
 export function Filters() {
   const { handleFilters } = useFilters();
+  const { data: projects } = useProjects();
   const [inputValue, setInputValue] = useState("");
   const [projectName, setProjectName] = useState<string | undefined>(undefined);
-  const projectNames = ["backend", "frontend - web", "ml service"];
+  const projectNames = projects?.map((project) => project.name.toLowerCase());
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setInputValue(e.target.value);
@@ -39,7 +41,7 @@ export function Filters() {
       handleProjectName(undefined);
       return;
     }
-    const name = projectNames.find(
+    const name = projectNames?.find(
       (name) => inputValue.length > 2 && name.includes(inputValue)
     );
 
