db1b21969448ef6a3b16a6372714b6e4c4b6f4ab
ari7946 <mercado7946@gmail.com>
2022-10-04 11:40:05 -0700

    Update changes

--- features/issues/api/use-issues.tsx
+++ features/issues/api/use-issues.tsx
@@ -1,28 +1,26 @@
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { useQuery, useQueryClient } from "react-query";
 import axios from "axios";
 import type { Page } from "@typings/page.types";
 import type { Issue } from "../types/issue.types";
+import { useFilters } from "@features/issues";
+import { IssueFilters } from "@features/issues";
 
-async function getIssues(page: number, filters: Filters) {
+async function getIssues(page: number, filters: IssueFilters) {
   const { data } = await axios.get(
     `https://prolog-api.profy.dev/issue?page=${page}`,
     { params: filters }
   );
-  console.log("data", data);
   return data;
 }
 
-type Filters = {
-  status: null | "open" | "resolved";
-  level: null | "error" | "warning" | "info";
-};
-
 export function useIssues(page: number) {
-  const [filters, setFilters] = useState<Filters>({
-    status: "open",
-    level: null,
-  });
+  const { filters } = useFilters();
+
+  useEffect(
+    () => console.log("update filters!"),
+    [filters.level, filters.project, filters.status]
+  );
 
   const query = useQuery<Page<Issue>, Error>(
     ["issues", page, filters],
@@ -32,7 +30,6 @@ export function useIssues(page: number) {
       staleTime: 60000,
     }
   );
-
   // Prefetch the next page!
   const queryClient = useQueryClient();
   useEffect(() => {
---
+++ features/issues/context/filters-context.tsx
@@ -0,0 +1,45 @@
+import React, {
+  useState,
+  useMemo,
+  useCallback,
+  createContext,
+  ReactNode,
+} from "react";
+import { IssueFilters } from "@features/issues";
+
+export const FiltersContext = createContext<{
+  filters: IssueFilters;
+  handleFilters: (filter: IssueFilters) => unknown;
+}>({
+  filters: { status: undefined, level: undefined, project: undefined },
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars, @typescript-eslint/no-empty-function
+  handleFilters: (_filter: IssueFilters) => {},
+});
+
+type FiltersProviderProps = {
+  children: ReactNode | ReactNode[];
+};
+
+export function FiltersProvider({ children }: FiltersProviderProps) {
+  const [filters, setFilters] = useState<IssueFilters>({
+    status: undefined,
+    level: undefined,
+    project: undefined,
+  });
+
+  const handleFilters = useCallback(
+    (filter) => setFilters((prevFilters) => ({ ...prevFilters, ...filter })),
+    []
+  );
+
+  const memoizedValue = useMemo(
+    () => ({ filters, handleFilters }),
+    [filters, handleFilters]
+  );
+
+  return (
+    <FiltersContext.Provider value={memoizedValue}>
+      {children}
+    </FiltersContext.Provider>
+  );
+}
