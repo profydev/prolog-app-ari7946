82dc0059de7b2eb1410a4ab5fe2e21458de9fc07
ari7946 <mercado7946@gmail.com>
2022-10-21 13:18:31 -0700

    Complete modal component and functionality

--- features/landing/components/contact-modal/contact-modal.tsx
+++ features/landing/components/contact-modal/contact-modal.tsx
@@ -1,7 +1,12 @@
-import React, { useState } from "react";
-import { color, displayFont, space, textFont } from "@styles/theme";
+import React, { useState, useRef } from "react";
+import { color, space, textFont } from "@styles/theme";
+import { Button, ButtonColor } from "@features/ui";
 import styled from "styled-components";
 import Modal from "react-modal";
+import { useClickAway } from "react-use";
+
+// (https://reactcommunity.org/react-modal/accessibility/)
+Modal.setAppElement("#contact-modal");
 
 const IconContainer = styled.div`
   position: fixed;
@@ -25,33 +30,100 @@ const customStyles = {
     bottom: "auto",
     marginRight: "-50%",
     transform: "translate(-50%, -50%)",
-    backgroundColor: "rgba(0,0,0,.9)",
+    backgroundColor: "#fff",
+    maxWidth: "400px",
+    height: "288px",
+    borderRadius: "12px",
+  },
+  overlay: {
+    backgroundColor: "rgba(0,0,0,.7)",
   },
 };
 
 const Icon = styled.img``;
 
+const EmailIcon = styled(Icon)`
+  color: black;
+  width: ${space(12)};
+  height: ${space(10)};
+`;
+
+const ModalContent = styled.div`
+  display: flex;
+  justify-content: space-evenly;
+  align-items: center;
+  flex-direction: column;
+  height: 100%;
+  padding-inline: ${space(6)};
+`;
+
+const Title = styled.p`
+  ${textFont("md", "semibold")}
+  font-size: ${space(5)};
+  color: ${color("gray", 900)};
+  margin: 0;
+`;
+
+const Text = styled.p`
+  ${textFont("sm", "regular")}
+  text-align: center;
+  line-height: ${space(5)};
+  color: ${color("gray", 500)};
+  margin: 0;
+`;
+
+const ModalButton = styled(Button)`
+  width: 100%;
+  font-size: ${space(4)};
+`;
+
+const ButtonGroup = styled.div`
+  display: flex;
+  gap: 1rem;
+  width: 100%;
+`;
+
 export function ContactModal() {
   const [isModalOpen, setIsModalOPen] = useState(false);
+  const ref = useRef(null);
 
   const toggle = () => setIsModalOPen(!isModalOpen);
 
+  // hides modal when user clicks outside the modal component
+  useClickAway(ref, () => {
+    toggle();
+  });
+
   return (
-    <>
-      <Modal
-        isOpen={isModalOpen}
-        shouldCloseOnEsc
-        shouldCloseOnOverlayClick
-        style={customStyles}
-      ></Modal>
-      <IconContainer
-        onClick={() => {
-          toggle();
-          window.open("mailto:support@prolog-app.com?subject=Support Request:");
-        }}
-      >
+    <div id="contact-modal" ref={ref}>
+      <Modal isOpen={isModalOpen} shouldCloseOnEsc style={customStyles}>
+        <ModalContent>
+          <EmailIcon src="/icons/email-modal.svg" />
+          <Title>Contact Us Via Email</Title>
+          <Text>
+            Any questions? Send us an email at prolog@profy.dev. We usually
+            answer within 24 hours.
+          </Text>
+          <ButtonGroup>
+            <ModalButton color={ButtonColor.gray} onClick={toggle}>
+              Cancel
+            </ModalButton>
+            <ModalButton
+              onClick={() => {
+                window.open(
+                  "mailto:support@prolog-app.com?subject=Support Request:"
+                );
+                toggle();
+              }}
+            >
+              Open Email App
+            </ModalButton>
+          </ButtonGroup>
+        </ModalContent>
+      </Modal>
+      <IconContainer onClick={toggle}>
         <Icon src="/icons/support.svg" />
       </IconContainer>
-    </>
+    </div>
   );
 }
